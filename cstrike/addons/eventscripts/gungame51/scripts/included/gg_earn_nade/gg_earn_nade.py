395bd9f6b74084e2a6ef89e6c7f5663c68d70c45
yournamehere <yournamehere@efb7a570-fe77-11de-bcdd-59c7212cd493>
2009-04-16 04:28:53 +0000

    gungame51/scripts/included/gg_earn_nade/gg_earn_nade.py: * Coded and mostly functional. No errors that I've seen as of yet, however, there is still more work to be done!

--- cstrike/addons/eventscripts/gungame51/scripts/included/gg_earn_nade/gg_earn_nade.py
+++ cstrike/addons/eventscripts/gungame51/scripts/included/gg_earn_nade/gg_earn_nade.py
@@ -9,14 +9,13 @@ $LastChangedDate: 2009-04-06 20:23:27 -0400 (Mon, 06 Apr 2009) $
 # ============================================================================
 # >> IMPORTS
 # ============================================================================
-# Python Imports
-
-
 # Eventscripts Imports
 import es
+import playerlib
 
 # GunGame Imports
 from gungame51.core.addons.shortcuts import AddonInfo
+from gungame51.core.players import Player
 
 # ============================================================================
 # >> ADDON REGISTRATION/INFORMATION
@@ -26,15 +25,6 @@ info.name = 'gg_earn_nade'
 info.title = 'GG Earn Grenade' 
 info.author = 'GG Dev Team' 
 info.version = '0.1'
-
-# ============================================================================
-# >> GLOBAL VARIABLES
-# ============================================================================
-
-
-# ============================================================================
-# >> CLASSES
-# ============================================================================
 
 
 # ============================================================================
@@ -49,8 +39,44 @@ def unload():
 # ============================================================================
 # >> GAME EVENTS
 # ============================================================================
+def player_death(event_var):
 
+    victimid = int(event_var['userid'])
+    attackerid = int(event_var['attacker'])
+    
+    # Make sure it wasn't a suicide
+    if attackerid == 0:
+        return
+        
+    # Make sure it wasn't a teamkill
+    if event_var['es_attackerteam'] == event_var['es_userteam']:
+        return
+        
+    # Is this player on nade level?
+    es.msg(Player(attackerid).weapon)
+    if Player(attackerid).weapon == 'hegrenade':
+    
+        # Make sure the player didn't kill with an hegrenade
+        if event_var['weapon'] == 'weapon_hegrenade':
+            return 
+        
+        # Are we in warmup?
+        if int(es.ServerVar("gg_warmup_round")) > 0 :
+            return
+
+        # Give them another grenade
+        giveGrenade(attackerid)
 
 # ============================================================================
 # >> CUSTOM/HELPER FUNCTIONS
 # ============================================================================
+def giveGrenade(userid):
+
+    # Does this player already have a grenade?
+    if int(playerlib.getPlayer(userid).get('he')) != 0:
+        # Don't give them one, return
+        return
+
+    cmdFormat = 'es_xgive %i weapon_hegrenade' % userid
+    es.server.queuecmd(cmdFormat)
+
