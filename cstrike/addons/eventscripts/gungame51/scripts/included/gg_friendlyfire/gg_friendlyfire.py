fd0c848e120af05bf85a70c02484c6733d5622a9
rideguy <rideguy@efb7a570-fe77-11de-bcdd-59c7212cd493>
2009-05-14 02:44:46 +0000

    gg_friendlyfire: -untested and missing the messaging.

--- cstrike/addons/eventscripts/gungame51/scripts/included/gg_friendlyfire/gg_friendlyfire.py
+++ cstrike/addons/eventscripts/gungame51/scripts/included/gg_friendlyfire/gg_friendlyfire.py
@@ -17,6 +17,7 @@ import es
 
 # GunGame Imports
 from gungame51.core.addons.shortcuts import AddonInfo
+from gungame51.core.leaders.shortcuts import getLeaderLevel
 
 # ============================================================================
 # >> ADDON REGISTRATION/INFORMATION
@@ -31,6 +32,9 @@ info.version = '0.1'
 # >> GLOBAL VARIABLES
 # ============================================================================
 
+gg_friendlyfire = es.ServerVar('gg_friendlyfire')
+mp_friendlyfire = es.ServerVar('mp_friendlyfire')
+oldFriendlyFire = int(mp_friendlyfire)
 
 # ============================================================================
 # >> CLASSES
@@ -46,10 +50,36 @@ def load():
 def unload():
     es.dbgmsg(0, 'Unloaded: %s' % info.name)
     
+    # Set friendlyfire back to what it was before gg_friendlyfire loaded
+    mp_friendlyfire.set(oldFriendlyFire)
+    
 # ============================================================================
 # >> GAME EVENTS
 # ============================================================================
 
+def es_map_start(event_var):
+    # Set mp_friendlyfire to 0
+    mp_friendlyfire.set(0)
+    
+def gg_start(event_var):
+    # Set mp_friendlyfire to 0
+    mp_friendlyfire.set(0)
+
+def gg_levelup(event_var):
+    # Get activation level
+    activateLevel = gungamelib.getTotalLevels()+1 - int(gg_friendlyfire)
+    
+    # If the Leader is on the friendlyfire level?
+    if getLeaderLevel() >= activateLevel:
+        # Check whether mp_friendlyfire is enabled
+        if int(mp_friendlyfire) == 0:
+            # Set mp_friendlyfire to 1
+            mp_friendlyfire.set(1)
+            '''
+            # Show message and sound
+            gungamelib.msg('gg_friendlyfire', '#all', 'WatchYourFire')
+            gungamelib.playSound('#all', 'friendlyfire')
+            '''
 
 # ============================================================================
 # >> CUSTOM/HELPER FUNCTIONS
