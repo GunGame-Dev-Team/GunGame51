a448909cb53e4a355162888f27007c4a45a2c781
rideguy <rideguy@efb7a570-fe77-11de-bcdd-59c7212cd493>
2009-05-14 19:25:13 +0000

    gg_dissolver.py: -grabbed a local instance of the cvar gg_dissolver so we don't use es.ServerVar() every player_death. -we now create the dissolver entity at round_start.  We trigger the dissolver on player_death.  We never Kill the entity, we just keep reusing it.
    This method is tested, but the code in this script is not.

--- cstrike/addons/eventscripts/gungame51/scripts/included/gg_dissolver/gg_dissolver.py
+++ cstrike/addons/eventscripts/gungame51/scripts/included/gg_dissolver/gg_dissolver.py
@@ -28,6 +28,12 @@ info.author = 'GG Dev Team'
 info.version = '0.1'
 
 # ============================================================================
+# >> GLOBAL VARIABLES
+# ============================================================================
+
+gg_dissolver = es.ServerVar("gg_dissolver")
+
+# ============================================================================
 # >> LOAD & UNLOAD
 # ============================================================================
 def load():
@@ -39,6 +45,10 @@ def unload():
 # ============================================================================
 # >> GAME EVENTS
 # ============================================================================
+def round_start(event_var):
+    
+    setupDissolver()
+
 def player_death(event_var):
 
     # Get userid
@@ -46,54 +56,36 @@ def player_death(event_var):
 
     # Dissolve ragdoll
     dissolveRagdoll(userid)
-    
 
 # ============================================================================
 # >> CUSTOM/HELPER FUNCTIONS
 # ============================================================================
+
+def setupDissolver():
+    
+    # Get a userid
+    userid = es.getuserid()
+    
+    #Give the dissolver entity and set some keyvalues
+    cmdFormat = 'es_xgive %s env_entity_dissolver; ' % userid
+    cmdFormat += 'es_xfire %s env_entity_dissolver AddOutput "target cs_ragdoll"; ' % userid
+    cmdFormat += 'es_xfire %s env_entity_dissolver AddOutput "magnitude 1"; ' % userid
+    es.server.queuecmd(cmdFormat)
+
 def dissolveRagdoll(userid):
 
     # Get dissolver effect
-    effect = int(es.ServerVar("gg_dissolver"))
+    effect = int(gg_dissolver)
 
     # Just remove the ragdoll?
     if effect == 1:
         es.delayed('2', 'es_xfire %s cs_ragdoll Kill' % userid)
     else:
-        # Give the entity dissolver and set its KeyValues
-        cmdFormat = 'es_xgive %s env_entity_dissolver; ' % userid
-        cmdFormat += 'es_xfire %s env_entity_dissolver AddOutput "target cs_ragdoll"; ' % userid
-        cmdFormat += 'es_xfire %s env_entity_dissolver AddOutput "magnitude 1"; ' % userid
-        
         # Check to see what effect to use
-        if effect == 6:
-            cmdFormat += 'es_xfire %s env_entity_dissolver AddOutput "dissolvetype %s"' % (userid, random.randint(0, 3))
+        if effect == 5:
+            es.server.queuecmd('es_xfire %s env_entity_dissolver AddOutput "dissolvetype %s"' % (userid, random.randint(0, 3)))
         else:
-            cmdFormat += 'es_xfire %s env_entity_dissolver AddOutput "dissolvetype %s"' % (userid, int(effect) - 1)
-        
-        es.server.queuecmd(cmdFormat)
+            es.server.queuecmd('es_xfire %s env_entity_dissolver AddOutput "dissolvetype %s"' % (userid, int(effect) - 2))
         
         # Dissolve the ragdoll then kill the dissolver
         es.delayed('0.01', 'es_xfire %s env_entity_dissolver Dissolve' % userid)
-        es.delayed('4', 'es_xfire %s env_entity_dissolver Kill' % userid)
-        es.delayed('4', 'es_xfire %s cs_ragdoll Kill' % userid)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
