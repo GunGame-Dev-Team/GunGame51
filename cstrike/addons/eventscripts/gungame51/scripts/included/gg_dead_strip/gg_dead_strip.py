cac66c0d2c5f9324b9b24062a8ad48b0f89d0eee
micbarr <micbarr@efb7a570-fe77-11de-bcdd-59c7212cd493>
2009-05-14 19:18:35 +0000

    scripts/included/gg_dead_strip/gg_dead_strip.py: * Removed "import gamethread" (we don't use it). * Removed "import playerlib" and replaced with "from playerlib import getPlayer". * Removed "import weaponlib" and replaced with "from weaponlib import getWeaponNameList". * Commented out "es.server.cmd('es_xfire %s game_weapon_manager AddOutput "maxpieces 0"' % es.getuserid())" and attempted to use the es.fire() method instead.   - Take note that I have tested this, and it seems to work, but I'd like to see more extensive testing on a live server. * Removed various instances of "playerlibPlayer = playerlib.getPlayer(userid)" and replaced with shorter declarations. * Removed various instances of "gungamePlayer = Player(userid)" and replaced with shorter declarations. * Replaced "if not playerlib.getPlayer(userid).get('he'):" with "if not getPlayer(userid).he:". * Replaced some single quotations with double quotations as per the request of RideGuy when dealing with strings.

--- cstrike/addons/eventscripts/gungame51/scripts/included/gg_dead_strip/gg_dead_strip.py
+++ cstrike/addons/eventscripts/gungame51/scripts/included/gg_dead_strip/gg_dead_strip.py
@@ -14,9 +14,8 @@ $LastChangedDate$
 
 # Eventscripts Imports
 import es
-import playerlib
-import gamethread
-import weaponlib
+from playerlib import getPlayer
+from weaponlib import getWeaponNameList
 
 # GunGame Imports
 from gungame51.core.addons.shortcuts import AddonInfo
@@ -34,7 +33,6 @@ info.version = '0.1'
 # ============================================================================
 # >> GLOBAL VARIABLES
 # ============================================================================
-
 gg_nade_bonus = es.ServerVar('gg_nade_bonus')
 
 # ============================================================================
@@ -56,18 +54,17 @@ def unload():
 # >> GAME EVENTS
 # ============================================================================
 def round_start(event_var):
-    
     # Remove all idle weapons that exist on the map.
-    es.server.cmd('es_xfire %s game_weapon_manager AddOutput "maxpieces 0"' % es.getuserid())
+    es.fire(es.getuserid(), 'game_weapon_manager AddOutput "maxpieces 0"')
+    #es.server.cmd('es_xfire %s game_weapon_manager AddOutput "maxpieces 0"' % es.getuserid())
 
 def item_pickup(event_var):
-
     # Get variables
     item = event_var['item']
     userid = int(event_var['userid'])
 
     # Is a weapon?
-    if ("weapon_" + item) not in weaponlib.getWeaponNameList():
+    if ("weapon_%s" %item) not in getWeaponNameList():
         return
         
     # Don't strip the knife
@@ -78,9 +75,8 @@ def item_pickup(event_var):
     if not es.exists('userid', userid):
         return
     
-    # Get player objects
-    gungamePlayer = Player(userid)
-    currentWeapon = gungamePlayer.weapon
+    # Get the player's GunGame weapon
+    currentWeapon = Player(userid).weapon
     
     # Check to see if the weapon is their gungame weapon or in their strip exceptions
     #if item == weapon or item in gungamePlayer.stripexceptions + ['flashbang', 'smokegrenade']:
@@ -91,14 +87,13 @@ def item_pickup(event_var):
     removeWeapon(userid, item)
 
     # If the player did not switch to the weapon they just picked up, no need to switch them back to their previous weapon
-    es.dbgmsg(0, "Weapon name is %s" % currentWeapon)
     if currentWeapon != item:
         return
     
     # Check if player is on nade level
     if weapon == 'hegrenade':
         # Switch the player knife if they are on nade level but don't have a nade
-        if not playerlib.getPlayer(userid).get('he'):
+        if not getPlayer(userid).he:
             es.sexec(userid, 'use weapon_knife')
             return
     
@@ -109,30 +104,23 @@ def item_pickup(event_var):
 # >> CUSTOM/HELPER FUNCTIONS
 # ============================================================================
 def removeWeapon(userid, item):
-
-    # Get weapon index
-    player = playerlib.getPlayer(userid)
-    weapon = player.attributes['weapon']
-    
     # Remove weapon
-    #cmdFormat = 'es_xremove %d' % player.get('weaponindex', item)
-    es.remove(player.get('weaponindex', item))
+    es.remove(getPlayer(userid).getWeaponIndex("weapon_%s" %item))
     
 def filterDrop(userid, args):
     # If command not drop, continue
     if args[0].lower() != 'drop':
         return 1
     
-    # Get player and their info
-    gungamePlayer = Player(userid)
-    weapon = gungamePlayer.weapon
+    # Get player's GunGame weapon
+    weapon = Player(userid).weapon
     
-    playerlibPlayer = playerlib.getPlayer(userid)
-    curWeapon = playerlibPlayer.attributes['weapon']
+    # Get the player's current weapon
+    curWeapon = getPlayer(userid).attributes['weapon']
     
     # Check to see if their current weapon is their level weapon
     if weapon != 'hegrenade':
-        return int(curWeapon != 'weapon_%s' % weapon)
+        return int(curWeapon != 'weapon_%s' %weapon)
     
     # ================
     # NADE BONUS CHECK
@@ -141,13 +129,13 @@ def filterDrop(userid, args):
     
     # Is nade bonus enabled?
     if nadeBonusWeapons[0] == '0':
-        return int(curWeapon != 'weapon_%s' % weapon)
+        return int(curWeapon != "weapon_%s" %weapon)
     
     # Loop through the nade bonus weapons
     for nadeWeapon in nadeBonusWeapons:
         # Prefix weapon_
         if not nadeWeapon.startswith('weapon_'):
-            nadeWeapon = 'weapon_%s' % nadeWeapon
+            nadeWeapon = "weapon_%s" %nadeWeapon
         
         # Don't allow them to drop it
         if nadeWeapon == curWeapon:
