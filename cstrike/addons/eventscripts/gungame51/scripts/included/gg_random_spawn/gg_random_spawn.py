4200c49ea6dc07f1cc616f9f417dad798eaa074a
rideguy <rideguy@efb7a570-fe77-11de-bcdd-59c7212cd493>
2009-05-15 19:18:45 +0000

    gg_random_spawn.py: -new addon to create random spawnpoints when gg_random_spawn is enabled. -UNTESTED
    gg_spawnpoints.py:
    -started but unfinished.

---
+++ cstrike/addons/eventscripts/gungame51/scripts/included/gg_random_spawn/gg_random_spawn.py
@@ -0,0 +1,107 @@
+# gungame/scripts/included/gg_random_points.py
+
+'''
+$Rev: 
+$LastChangedBy: 
+$LastChangedDate: 
+'''
+
+# ============================================================================
+# >> IMPORTS
+# ============================================================================
+# Eventscripts Imports
+import os
+
+# GunGame Imports
+import es
+
+# ============================================================================
+# >> ADDON REGISTRATION/INFORMATION
+# ============================================================================
+info = AddonInfo()
+info.name = 'gg_random_spawn'
+info.title = 'GG Random Spawn' 
+info.author = 'GG Dev Team' 
+info.version = '0.1'
+
+# ============================================================================
+# >> GLOBAL VARIABLES
+# ============================================================================
+
+spawnPoints = []
+pointsLoaded = 0
+
+# ============================================================================
+# >> LOAD & UNLOAD
+# ============================================================================
+def load():
+    es.dbgmsg(0, 'Loaded: %s' % info.name)
+    
+def unload():
+    es.dbgmsg(0, 'Unloaded: %s' % info.name)
+    
+# ============================================================================
+# >> GUNGAME EVENTS
+# ============================================================================
+
+def es_map_start(event_var):
+
+    loadSpawnFile(event_var['mapname'])
+
+def player_activate(event_var):
+    global pointsLoaded
+    
+    if pointsLoaded:
+        return
+    
+    pointsLoaded = 1
+    
+    if not spawnPoints:
+        return
+    
+    loadRandomPoints(event_var['userid'])
+
+# ============================================================================
+# >> CUSTOM/HELPER FUNCTIONS
+# ============================================================================
+
+def loadSpawnFire(mapname):
+    global spawnPoints
+    global pointsLoaded
+    
+    spawnPoints = []
+    pointsLoaded = 0
+    
+    # Get spawnpoint file
+    self.spawnFile = '%s/%s/%s.txt' % mapname.replace('\\', '/'), spawnPointDirectory, self.mapName)
+    
+    # Does the file exist?
+    if not self.exists():
+        return
+    
+    # Get spawnpoint lines
+    spawnPointFile = open(self.spawnFile, 'r')
+    fileLines = [x.strip() for x in spawnPointFile.readlines()]
+    spawnPointFile.close()
+    
+    # Set up spawnpoints
+    spawnPoints = [x.split(' ', 6) for x in fileLines]
+
+def loadRandomPoints(userid):
+    
+    # Remove existing spawnpoints
+    for tSpawn in es.createentitylist('info_player_terrorist'):
+        es.server.cmd('es_xremove info_player_terrorist')
+    for ctSpawn in es.createentitylist('info_player_counterterrorist'):
+        es.server.cmd('es_xremove info_player_counterterrorist')
+    
+    # Loop through the spawnpoints
+    for spawn in spawnPoints:
+        for team in ('info_player_terrorist', 'info_player_counterterrorist'):
+            # Create the spawnpoint and get the index
+            es.server.cmd('es_xgive %s %s' % (userid, team))
+            index = int(es.ServerVar('eventscripts_lastgive'))
+            
+            # Set the spawnpoint position and rotation
+            es.setindexprop(index, 'CBaseEntity.m_vecOrigin', '%s,%s,%s' % (spawn[0], spawn[1], spawn[2]))
+            es.setindexprop(index, 'CBaseEntity.m_angRotation', '0,%s,0' % spawn[4])
--- cstrike/addons/eventscripts/gungame51/scripts/included/gg_spawnpoints/gg_spawnpoints.py
+++ cstrike/addons/eventscripts/gungame51/scripts/included/gg_spawnpoints/gg_spawnpoints.py
@@ -14,9 +14,12 @@ $LastChangedDate$
 
 # Eventscripts Imports
 import es
+from cmdlib import registerClientCommand
+from cmdlib import unregisterClientCommand
 
 # GunGame Imports
 from gungame51.core.addons.shortcuts import AddonInfo
+from gungame51.core.messaging.shortcuts import msg
 
 # ============================================================================
 # >> ADDON REGISTRATION/INFORMATION
@@ -26,6 +29,7 @@ info.name = 'gg_spawnpoints'
 info.title = 'GG Spawnpoints' 
 info.author = 'GG Dev Team' 
 info.version = '0.1'
+info.translations = ['gg_spawnpoints']
 
 # ============================================================================
 # >> GLOBAL VARIABLES
@@ -43,9 +47,21 @@ info.version = '0.1'
 def load():
     es.dbgmsg(0, 'Loaded: %s' % info.name)
     
+    registerClientCommand('spawn_add', cmd_spawn_add, 'Adds a spawnpoint at the users location', 'ADMIN')
+    registerClientCommand('spawn_remove', cmd_spawn_remove, 'Remove spawnpoint at index', 'ADMIN')
+    registerClientCommand('spawn_remove_all', cmd_spawn_remove_all, 'Removes all spawn points', 'ADMIN')
+    registerClientCommand('spawn_show', cmd_spawn_show, 'Toggles spawn point models on and off', 'ADMIN')
+    registerClientCommand('spawn_print', cmd_spawn_print, 'Prints spawnpoints by index into the players console', 'ADMIN')
+    
 def unload():
     es.dbgmsg(0, 'Unloaded: %s' % info.name)
     
+    unregisterClientCommand('spawn_add')
+    unregisterClientCommand('spawn_remove')
+    unregisterClientCommand('spawn_remove_all')
+    unregisterClientCommand('spawn_show')
+    unregisterClientCommand('spawn_print')
+
 # ============================================================================
 # >> GAME EVENTS
 # ============================================================================
@@ -54,3 +70,17 @@ def unload():
 # ============================================================================
 # >> CUSTOM/HELPER FUNCTIONS
 # ============================================================================
+
+def cmd_spawn_add(userid):
+    global spawnPoints
+    
+    # Get player info
+    playerlibPlayer = playerlib.getPlayer(userid)
+    playerLoc = es.getplayerlocation(userid)
+    playerViewAngle = playerlibPlayer.get('viewangle')
+    
+    # Create spawnpoint
+    addSpawnPoint(playerLoc[0], playerLoc[1], playerLoc[2], playerViewAngle[1])
+    
+    msg(userid, 'AddedSpawnpoint', {'index': spawnPoints.getTotalPoints()-1})
+
