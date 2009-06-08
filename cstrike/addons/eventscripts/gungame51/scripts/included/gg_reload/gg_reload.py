30c93d680ceb63406f56912097e5caa30fc6e319
micbarr <micbarr@efb7a570-fe77-11de-bcdd-59c7212cd493>
2009-06-08 20:36:25 +0000

    scripts/included/gg_reload/gg_reload.py: * Changed the variable name "attackerid" to "attacker". * Changed the variable name "victimid" to "userid". * Changed "import weaponlib" to "from weaponlib import getWeapon" for consistency. * Added "from playerlib import getPlayer". * Changed the es.setplayerprop() for setting the weapon's clip to use the playerlib method.

--- cstrike/addons/eventscripts/gungame51/scripts/included/gg_reload/gg_reload.py
+++ cstrike/addons/eventscripts/gungame51/scripts/included/gg_reload/gg_reload.py
@@ -14,7 +14,8 @@ $LastChangedDate$
 
 # Eventscripts Imports
 import es
-import weaponlib
+from weaponlib import getWeapon
+from playerlib import getPlayer
 
 # GunGame Imports
 from gungame51.core.addons.shortcuts import AddonInfo
@@ -34,51 +35,51 @@ info.version = '0.1'
 # ============================================================================
 def load():
     es.dbgmsg(0, 'Loaded: %s' % info.name)
-    
+
 def unload():
     es.dbgmsg(0, 'Unloaded: %s' % info.name)
-    
+
 # ============================================================================
 # >> GAME EVENTS
 # ============================================================================
 
 def player_death(event_var):
     # Get the userids of the attacker and victim
-    attackerid = int(event_var['attacker'])
-    victimid = int(event_var['userid'])
+    attacker = int(event_var['attacker'])
+    userid = int(event_var['userid'])
     
     # If there is no attacker (falling to death), return
-    if not attackerid:
+    if not attacker:
         return
-    
+
     # If the kill was a suicide, return
-    if attackerid == victimid:
+    if attacker == userid:
         return
 
     # If the kill was a teamkill, return
     if event_var['es_attackerteam'] == event_var['es_userteam']:
         return
-    
+
     # Get the name of the weapon used to get the kill
     weapon = event_var['weapon']
-    
+
     # If the weapon name doesn't match the player's level's weapon name, return
-    if weapon != Player(attackerid).weapon:
+    if weapon != Player(attacker).weapon:
         return
-    
+
     # If the player is on hegrenade or knife level, return
     if weapon in ('hegrenade', 'knife'):
         return
-    
+
     # Get the weapon object and the size if its clip
-    weaponObject = weaponlib.getWeapon(weapon)
-    clip = weaponObject.clip
-    
+    weaponObject = getWeapon(weapon)
+
     # Find the attacker's weapon index to be used to reload the weapon
-    playerHandle = es.getplayerhandle(attackerid)
+    playerHandle = es.getplayerhandle(attacker)
+
     for index in weaponObject.indexlist:
         # When the attacker's handle matches the index handle we have found the attacker's weapon index
         if es.getindexprop(index, 'CBaseEntity.m_hOwnerEntity') == playerHandle:
-            # When a match is found, reload the clip
-            es.setindexprop(index, 'CBaseCombatWeapon.LocalWeaponData.m_iClip1', clip)
+            # Set the clip to the maximum ammo allowed
+            getPlayer(attacker)['clip'][weaponObject] = weaponObject.clip
             break
