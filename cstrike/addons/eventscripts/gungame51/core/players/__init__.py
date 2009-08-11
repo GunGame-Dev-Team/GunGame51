2e2b90110eef692aa6696d8090e99277b95cf126
micbarr <micbarr@efb7a570-fe77-11de-bcdd-59c7212cd493>
2009-08-11 20:50:42 +0000

    ./core/cfg/files/gg_map_vote.py: * Added a cfglib.AddonCFG instance for "gg_map_vote.cfg" so that reloading GunGame would not raise an error.
    ./core/players/__init__.py:
    * Added "from afk import AFK".
    * Added "from options import PlayerOptions".
    * Added "self.options = PlayerOptions()" to the BasePlayer() class.
    * Removed / "broke out" the AFK() class to its own module (core/players/afk.py).
    
    ./core/players/afk.py:
    * Added.
    * This moved the AFK() class from the "core/players/__init__.py".
    
    ./core/players/options.py:
    * Added the class PlayerOptions() for "BasePlayer(userid).options = PlayerOptions()".
    * Added the class Options() to hold the individual player options.
    
    
    ADDITIONAL NOTES:
    This was primarily a commit to set up for custom player options. The Options() class automatically initializes 2 attributes: "title" and "popup". "Title" is the title as the option would be shown in a menu. "Popup" would be the popuplib instance for the popup. Here is an example of the "Sounds" options:
    
    ggPlayer = Player(userid)
    ggPlayer.options.sound = Options()
    ggPlayer.options.sound.title = 'Sounds'
    ggPlayer.options.sound.popup = popuplib.find('gg_sounds_menu')
    ggPlayer.options.sound.pack = 'default'

--- cstrike/addons/eventscripts/gungame51/core/players/__init__.py
+++ cstrike/addons/eventscripts/gungame51/core/players/__init__.py
@@ -22,6 +22,8 @@ from gungame51.core.weapons.shortcuts import getLevelMultiKill
 from gungame51.core import getOS
 from gungame51.core import GunGameError
 from gungame51.core.messaging import __messages__
+from options import PlayerOptions
+from afk import AFK
 
 # ============================================================================
 # >> GLOBALS
@@ -84,6 +86,7 @@ class CustomAttributeCallbacks(dict):
 
 setHooks = CustomAttributeCallbacks()
 
+
 class PreventLevel(list):
     '''
     Class designed to handle the PreventLevel player attribute. This class is a
@@ -110,99 +113,6 @@ class PreventLevel(list):
             list.remove(self, name)
 
 
-class AFK(object):
-    def __init__(self, userid):
-        self.userid = int(userid)
-        self.total = None
-        self.rounds = 0
-
-    def __call__(self):
-        return (self.total == self.calculate())
-
-    def reset(self):
-        '''Resets a players AFK math total.'''
-        # Check the player exists
-        if not es.exists('userid', self.userid):
-            return
-
-        # Update the AFK math total
-        self.total = self.calculate()
-
-    def calculate(self):
-        # Check the player exists
-        if not es.exists('userid', self.userid):
-            return
-
-        # Get the player's location
-        x, y, z = es.getplayerlocation(self.userid)
-
-        return int(x) + int(y) + int(es.getplayerprop(self.userid,
-                   'CCSPlayer.m_angEyeAngles[0]')) + \
-                    int(es.getplayerprop(self.userid, 'CCSPlayer.m_angEyeAngles[1]'))
-
-    def isActive(self):
-        '''
-        Sets the player to a state that is NOT AFK.
-        Only used when we know that the player is active and NOT AFK.
-        
-        Example:
-            event player_jump
-        '''
-        # Make sure player is on a team
-        if isSpectator(self.userid):
-            raise ValueError('Unable to make player active (%s): not on a team.' % self.userid)
-
-        # Reset player math total
-        self.total = 0
-        self.afkrounds = 0
-    
-    def teleport(self, x, y, z, eyeangle0=0, eyeangle1=0):
-        '''
-        Teleport the player.
-
-        Recalculates the player's location automatically for the scripter.
-        '''
-        # Make sure player is on a team
-        if isSpectator(self.userid):
-            raise ValueError('Unable to teleport player (%s): not on a team.'
-                %self.userid)
-
-        # Make sure the player is alive
-        if isDead(self.userid):
-            raise ValueError('Unable to teleport player (%s): not alive.'
-                %self.userid)
-
-        # Set position
-        es.server.queuecmd('es_xsetpos %d %s %s %s' % (self.userid, x, y, z))
-
-        # Set eye angles
-        if eyeangle0 != 0 or eyeangle1 != 0:
-            es.server.queuecmd('es_xsetang %d %s %s' %(self.userid, eyeangle0,
-                                                       eyeangle1))
-
-        # Reset player AFK calculation
-        gamethread.delayed(0.1, self.reset, ())
-
-    def eyeangles(self, eyeAngle0=0, eyeAngle1=0):
-        '''Sets a players view angle.'''
-        # Make sure player is on a team
-        if isSpectator(self.userid):
-            raise ValueError('Unable to set player angles (%s): not on a team'
-                %self.userid)
-
-        # Make sure player is alive
-        if isDead(self.userid):
-            raise ValueError('Unable to set player angles (%s): not alive.'
-                %self.userid)
-
-        # Set angles
-        es.server.queuecmd('es_xsetang %d %s %s' %(self.userid, eyeangle0,
-                                                   eyeangle1))
-
-        # Reset player AFK calculation
-        gamethread.delayed(0.1, self.reset, ())
-
-
 class BasePlayer(object):
     # =========================================================================
     # >> BasePlayer() CLASS INITIALIZATION
@@ -217,6 +127,7 @@ class BasePlayer(object):
         self.index = int(getPlayer(str(self.userid)).index)
         self.weapon = self.getWeapon()
         self.stripexceptions = []
+        self.options = PlayerOptions()
 
     # =========================================================================
     # >> BasePlayer() CLASS ATTRIBUTE METHODS
---
+++ cstrike/addons/eventscripts/gungame51/core/players/options.py
@@ -0,0 +1,72 @@
+# ../addons/eventscripts/gungame/core/players/options.py
+
+'''
+$Rev$
+$LastChangedBy$
+$LastChangedDate$
+'''
+
+# ============================================================================
+# >> CLASSES
+# ============================================================================
+class PlayerOptions(object):
+    '''
+    Class that controls/stores custom player options.
+    '''
+    # =========================================================================
+    # >> PlayerOptions() CLASS ATTRIBUTE METHODS
+    # =========================================================================
+    def __setattr__(self, name, value):
+        # Set the attribute value
+        object.__setattr__(self, name, value)
+
+    def __getattr__(self, name):
+        # Return the attribute value
+        return object.__getattribute__(self, name)
+
+    def __setitem__(self, name, value):
+        # Forward to __setattr__
+        self.__setattr__(name, value)
+
+    def __getitem__(self, name):
+        # Return using __getattr__
+        return self.__getattr__(name)
+
+
+class Options(object):
+    '''
+    Class used for custom individual player options.
+    
+    Example:
+        ggPlayer = Player(userid)
+        ggPlayer.options.sound = Options()
+        ggPlayer.options.sound.title = 'Sounds'
+        ggPlayer.options.sound.popup = popuplib.find('gg_sounds_menu')
+        ggPlayer.options.sound.pack = 'default'
+    '''
+    # =========================================================================
+    # >> Options() CLASS INITIALIZATION
+    # =========================================================================
+    def __init__(self):
+        # Define the title
+        self.title = None
+        self.popup = None
+
+    # =========================================================================
+    # >> OPTIONS() CLASS ATTRIBUTE METHODS
+    # =========================================================================
+    def __setattr__(self, name, value):
+        # Set the attribute value
+        object.__setattr__(self, name, value)
+
+    def __getattr__(self, name):
+        # Return the attribute value
+        return object.__getattribute__(self, name)
+
+    def __setitem__(self, name, value):
+        # Forward to __setattr__
+        self.__setattr__(name, value)
+
+    def __getitem__(self, name):
+        # Return using __getattr__
+        return self.__getattr__(name)
