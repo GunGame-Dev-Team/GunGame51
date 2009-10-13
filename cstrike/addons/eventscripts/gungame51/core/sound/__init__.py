6c3089de3009aba1b60757f5ad18f250505d55d5
micbarr <micbarr@efb7a570-fe77-11de-bcdd-59c7212cd493>
2009-10-13 22:47:03 +0000

    ./core/sound/__init__.py: * Added functionality for random sounds.
    ./cfg/gungame51/sound_packs/random_sound_files:
    * Added.
    
    NOTE TO DEVS:
    "./cfg/gungame51/sound_packs/random_sound_files" is the location of where you will put text files that contain the random sounds. Just like GunGame5.0, you list these without the "sound/" extenstion at the front (it is assumed). For the INI file, you simply put the name of the text file you want to read from that is located in the "./cfg/gungame51/sound_packs/random_sound_files" directory/folder.

--- cstrike/addons/eventscripts/gungame51/core/sound/__init__.py
+++ cstrike/addons/eventscripts/gungame51/core/sound/__init__.py
@@ -12,6 +12,7 @@ $LastChangedDate$
 # Python Imports
 from configobj import ConfigObj
 from path import path
+from random import choice
 
 # EventScripts Imports
 import es
@@ -37,15 +38,50 @@ class SoundPack(object):
 
     def __getitem__(self, name):
         if self.__pack__.has_key(name):
+            # See if this is a random sound file
+            if self._is_random(self.__pack__[name]):
+                # Return the random sound from the file
+                return self.get_random_sound(self.__pack__[name])
+
+            # Return the sound name
             return self.__pack__[name]
         else:
             return None
 
     def __getattr__(self, name):
         if self.__pack__.has_key(name):
+            # See if this is a random sound file
+            if self._is_random(self.__pack__[name]):
+                # Return the random sound from the file
+                return self.get_random_sound(self.__pack__[name])
+
+            # Return the sound name
             return self.__pack__[name]
         else:
             return None
+
+    def _is_random(self, name):
+        return (name.endswith('.txt'))
+
+    def _random_exists(self, name):
+        return (path.isfile(iniDir.joinpath('random_sound_files/%s' % name)))
+
+    def get_random_sound(self, name):
+        # Make sure the random sound file exists
+        if not self._random_exists(name):
+            return None
+
+        # Open the random sound file
+        randomFile = open(iniDir.joinpath('random_sound_files/%s' % name))
+
+        # Select a random sound from the list
+        randomSounds = [x.strip('\\n').strip() for x in randomFile.readlines()]
+
+        # Close the random file
+        randomFile.close()
+
+        # Return the randomly selected sound if the list is not empty
+        return choice(randomSounds) if randomSounds else None
 
 def make_downloadable():
     # Make sure we are in a map
@@ -68,6 +104,32 @@ def make_downloadable():
                 continue
 
             # Make sure that the sound file exists at the given path
-            if path.isfile(soundDir.joinpath(config[name])):
+            if sound_exists(config[name]):
                 # Make the sound downloadable
                 es.stringtable('downloadables', 'sound/%s' %config[name])
+            else:
+                # See if the file is a random sound text file
+                if not path.isfile(iniDir.joinpath('random_sound_files/%s'
+                    %config[name])):
+
+                    continue
+
+                # Open the random sound file
+                randomFile = open(iniDir.joinpath('random_sound_files/%s'
+                    %config[name]))
+
+                # Loop through all sounds in the file
+                for sound in randomFile.readlines():
+                    # Remove the line return character and whitespace,
+                    sound = sound.strip('\\n').strip()
+
+                    # Make sure that the sound file exists at the given path
+                    if sound_exists(sound):
+                        # Make the sound downloadable
+                        es.stringtable('downloadables', 'sound/%s' %sound)
+                            
+                # Close the random sound file
+                randomFile.close()
+                        
+def sound_exists(sound):
+    return (path.isfile(soundDir.joinpath(sound)))
