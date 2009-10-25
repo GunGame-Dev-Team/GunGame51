98dfd57b68ea708dcae8c3f1350db6d6a401f645
WarrenAlpert <WarrenAlpert@efb7a570-fe77-11de-bcdd-59c7212cd493>
2009-10-25 17:14:43 +0000

    gg_turbo.py: * Added support for gg_handicap_update level changes

--- cstrike/addons/eventscripts/gungame51/scripts/included/gg_turbo/gg_turbo.py
+++ cstrike/addons/eventscripts/gungame51/scripts/included/gg_turbo/gg_turbo.py
@@ -18,6 +18,8 @@ from weaponlib import getWeaponNameList
 # GunGame Imports
 from gungame51.core.addons.shortcuts import AddonInfo
 from gungame51.core.players.shortcuts import Player
+from gungame51.core.players.shortcuts import addAttributeCallBack
+from gungame51.core.players.shortcuts import removeCallBacksForAddon
 from gungame51.core.weapons.shortcuts import getLevelWeapon
 
 # ============================================================================
@@ -39,6 +41,7 @@ info.version = '0.1'
 # >> LOAD & UNLOAD
 # ============================================================================
 def load():
+    addAttributeCallBack('level', levelCallback, info.name)
     es.dbgmsg(0, 'Loaded: %s' % info.name)
 
 def unload():
@@ -52,19 +55,19 @@ def gg_levelup(event_var):
     userid = int(event_var['leveler'])
 
     # Strip and give weapon
-    giveWeapon(userid)
+    giveWeapon(userid, int(event_var['old_level']))
 
 def gg_leveldown(event_var):
 
     userid = int(event_var['leveler'])
 
     # Strip and give weapon
-    giveWeapon(userid)
+    giveWeapon(userid, int(event_var['old_level']))
 
 # ============================================================================
 # >> CUSTOM/HELPER FUNCTIONS
 # ============================================================================
-def giveWeapon(userid):
+def giveWeapon(userid, previousLevel):
 
     # Do player checks first
     if not playerChecks(userid):
@@ -77,7 +80,7 @@ def giveWeapon(userid):
     player.giveWeapon()
     
     # Get the names of the previous and current level weapons
-    previousWeapon = "weapon_%s" % getLevelWeapon(int(player.level) - 1)
+    previousWeapon = "weapon_%s" % getLevelWeapon(previousLevel)
     currentWeapon = "weapon_%s" % player.weapon
     
     # Retrieve a playerlib.Player() instance
@@ -118,3 +121,34 @@ def playerChecks(userid):
         return False
 
     return True
+
+def levelCallback(name, value, ggPlayer):
+    # If the player has been assigned a level already
+    if hasattr(ggPlayer, 'level'):
+        # Get their previous level, before it was changed
+        previousLevel = ggPlayer.level
+    # Otherwise
+    else:
+        # Assume they are on level 1
+        previousLevel = 1
+
+    # Delay the check to see if they have the correct turbo weapon
+    gamethread.delayed(0.05, checkCallBackWeapon, (ggPlayer, previousLevel))
+
+def checkCallBackWeapon(ggPlayer, previousLevel):
+    weapon = 'weapon_%s' % ggPlayer.weapon
+
+    # Retrieve a playerlib.Player() instance
+    pPlayer = getPlayer(ggPlayer.userid)
+
+    # Get the weapon that is in the slot that their level's weapon should be in
+    if weapon in list_pWeapons:
+        weaponHeld = pPlayer.getPrimary()
+    elif weapon in list_sWeapons:
+        weaponHeld = pPlayer.getSecondary()
+    else:
+        return
+
+    # If they are they holding the previous level's weapon, remove it
+    if weapon != weaponHeld:
+        giveWeapon(ggPlayer.userid, previousLevel)
