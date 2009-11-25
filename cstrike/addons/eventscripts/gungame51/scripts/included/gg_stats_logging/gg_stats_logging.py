7cd2f5b1bea0f718defa1e7a6ed37c8626e26e89
micbarr <micbarr@efb7a570-fe77-11de-bcdd-59c7212cd493>
2009-11-25 07:49:47 +0000

    ./scripts/included/gg_stats_logging/gg_stats_logging.py: * Completed the port from GunGame5.0 to GunGame5.1.
    ./cfg/gungame51/included_addon_configs/gg_stats_logging.txt:
    * Added.

--- cstrike/addons/eventscripts/gungame51/scripts/included/gg_stats_logging/gg_stats_logging.py
+++ cstrike/addons/eventscripts/gungame51/scripts/included/gg_stats_logging/gg_stats_logging.py
@@ -10,13 +10,14 @@ $LastChangedDate$
 # >> IMPORTS
 # ============================================================================
 # Python Imports
-
+from __future__ import with_statement
 
 # Eventscripts Imports
 import es
 
 # GunGame Imports
 from gungame51.core.addons.shortcuts import AddonInfo
+from gungame51.core import getGameDir
 
 # ============================================================================
 # >> ADDON REGISTRATION/INFORMATION
@@ -30,27 +31,76 @@ info.version = '0.1'
 # ============================================================================
 # >> GLOBAL VARIABLES
 # ============================================================================
-
-
-# ============================================================================
-# >> CLASSES
-# ============================================================================
-
+list_events = []
 
 # ============================================================================
 # >> LOAD & UNLOAD
 # ============================================================================
 def load():
+    global list_events
+
+    this = __import__(__name__)
+
+    # Get the file
+    with open(getGameDir('cfg/gungame51/included_addon_configs/' + \
+        'gg_stats_logging.txt'), 'r') as f:
+
+        # Create a list of lines in the file
+        list_lines = [x.strip() for x in f.readlines()]
+
+        # Remove commented out and blank lines
+        list_lines = filter(lambda x: not x.startswith('//') and x, list_lines)
+
+        # Loop through all lines in the file
+        for line in list_lines:
+            # Register this addon for the specified event
+            es.addons.registerForEvent(this, line, logEvent)
+
+            # Add the event to the list of events
+            list_events.append(line)
+
     es.dbgmsg(0, 'Loaded: %s' % info.name)
-    
+
 def unload():
+    global list_events
+
+    this = __import__(__name__)
+
+    # Loop through each named event in the list of events
+    for event in list_events:
+        # Unregister for the event
+        es.addons.unregisterForEvent(this, event)
+
     es.dbgmsg(0, 'Unloaded: %s' % info.name)
-    
-# ============================================================================
-# >> GAME EVENTS
-# ============================================================================
 
-
-# ============================================================================
+# ==============================================================================
 # >> CUSTOM/HELPER FUNCTIONS
-# ============================================================================
+# ==============================================================================
+def logEvent(event_var):
+    # Get event info
+    event = event_var['es_event']
+    userid = event_var['userid']
+
+    if event in ('gg_levelup', 'gg_knife_steal', 'gg_win'):
+        userid = event_var['attacker']
+
+    # Make sure the player exists
+    if not es.exists('userid', userid):
+        return
+
+    # Get player data
+    playerName = es.getplayername(userid)
+    steamid = es.getplayersteamid(userid)
+    teamName = getTeamName(es.getplayerteam(userid))
+
+    # Log it
+    es.server.queuecmd('es_xlogq "%s<%s><%s><%s>" triggered "%s"' 
+        %(playerName, userid, steamid, teamName, event))
+
+def getTeamName(team):
+    if team == 2:
+        return 'TERRORIST'
+    elif team == 3:
+        return 'CT'
+    else:
+        return 'UNKNOWN'
