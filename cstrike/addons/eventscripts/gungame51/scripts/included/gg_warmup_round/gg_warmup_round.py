c522d8b7ef4c3ae6be5a929443e2b2db21118bcc
micbarr <micbarr@efb7a570-fe77-11de-bcdd-59c7212cd493>
2010-01-17 20:24:54 +0000

    ./scripts/included/gg_warmup_round/gg_warmup_round.py: * Removed whitespace. * Added checks for gg_warmup_elimination and gg_warmup_deathmatch to fix a but to where if one or the other was previously loaded, it did not get reloaded. * Fixed a bug to where gg_deathmatch was reloaded instead of gg_elimination. * Added comments.

--- cstrike/addons/eventscripts/gungame51/scripts/included/gg_warmup_round/gg_warmup_round.py
+++ cstrike/addons/eventscripts/gungame51/scripts/included/gg_warmup_round/gg_warmup_round.py
@@ -64,59 +64,61 @@ GG_WARMUP_EXTRA_TIME = 3
 def load():
     # Load warmup round (delayed)
     gamethread.delayed(1, do_warmup)
-    
+
     # Loaded message
     es.dbgmsg(0, 'Loaded: %s' % info.name)
-    
+
     # Register a server command to to cancel an in-progress warmup round
     cmdlib.registerServerCommand('gg_end_warmup', servercmd_end_warmup,
     	"Immediately ends the warmup round if there is one in progress.")
-    
+
 def unload():
     # Deleting warmup timer
     warmupCountDown = repeat.find('gungameWarmupTimer') 
+
+    # If the warmup timer exists, stop and delete it
     if warmupCountDown:    
         warmupCountDown.stop()
         warmupCountDown.delete()
 
     # Reset server vars
     reset_server_vars(True)
-    
+
     # Unload message
     es.dbgmsg(0, 'Unloaded: %s' % info.name)
-    
+
     # Unregister the server command that cancels an in-progress warmup round
     cmdlib.unregisterServerCommand('gg_end_warmup')
-    
+
 # ============================================================================
 # >> COMMAND CALLBACKS
 # ============================================================================
 def servercmd_end_warmup(args): # args are ignored, but needed for server cmd
     # Get the timer/repeat instance
     warmupCountDown = repeat.find('gungameWarmupTimer')
-    
+
     # Break out if the timer is invalid (implies warmup is not in progress)
     if not warmupCountDown:
         return
-    
+
     # Stop the round from being able to end
     disable_round_end()
-    
+
     # Restart the round ourselves in 1 second
     es.server.queuecmd('mp_restartgame 1')
-    
+
     # Before the round ends up restarting, prepare gungame to be ready
     # for the first round of play
     gamethread.delayed(0.8, prepare_game)
-    
+
     # End the warmup round
     # Delayed to allow mp_restartgame to strip all weapons before gg gives them
     # (Emulating behavior of call to mp_restartgame in count_down() below)
     gamethread.delayed(1, end_warmup, ('Warmup_End_Forced'))
-    
+
     # Display a chat message with the same message
     msg("#human", 'Warmup_End_Forced', prefix=True)
-    
+
 # ============================================================================
 # >> GAME EVENTS
 # ============================================================================
@@ -137,21 +139,21 @@ def es_map_start(event_var):
 
     # Start warmup timer
     do_warmup()
-        
+
 def hegrenade_detonate(event_var):
     # Making sure warmup round is running
     warmupCountDown = repeat.find('gungameWarmupTimer')
-    
+
     if not warmupCountDown:
         return
 
     # Get player userid and player object
     userid = int(event_var['userid'])
-    
+
     # Is the client on the server?
     if not es.exists('userid', userid):
         return
-    
+
     # Give user a hegrenade, if eligable
     if int(event_var['es_userteam']) > 1 and not getPlayer(userid).isdead and \
         str(gg_warmup_weapon) == 'hegrenade':
@@ -160,44 +162,44 @@ def hegrenade_detonate(event_var):
 def player_spawn(event_var):
     # Making sure warmup round is running
     warmupCountDown = repeat.find('gungameWarmupTimer')
-    
+
     if not warmupCountDown:
         return
-        
+
     userid = int(event_var['userid'])
 
     # Is a spectator or dead?
     if int(event_var['es_userteam']) < 2 or getPlayer(userid).isdead:
         return
-    
+
     # If it is the last split second before mp_restartgame fires, protect the
     # player
     if not 'gg_warmup_round' in PriorityAddon():
         getPlayer(userid).godmode = 1
-    
+
     # Get player object
     ggPlayer = Player(userid)
-    
+
     # Check if the warmup weapon is the level 1 weapon
     if str(gg_warmup_weapon) in ('0', '', '0.0'):
         ggPlayer.give_weapon()
         return
-    
+
     # Check if the warmup weapon is a knife
     if str(gg_warmup_weapon) == 'knife':
         es.sexec(userid, 'use weapon_knife')
         return
-    
+
     '''>> Note to Developers:
             This delay arrangement was needed because it was crashing
             the server when bots spawned during a warmup round.
     '''
 
     delay = 0.07
-    
+
     if es.isbot(userid):
         delay += 0.25
-    
+
     # Strip the player's weapons (split second delay)
     gamethread.delayed(delay, ggPlayer.strip, (True, [gg_warmup_weapon]))
 
@@ -228,43 +230,43 @@ def do_warmup(useBackupVars=True):
         mp_freezetime_backup = int(mp_freezetime)
         gg_deathmatch_backup = int(gg_deathmatch)
         gg_elimination_backup = int(gg_elimination)
-        
+
     # Setting mp_freezetime
     mp_freezetime.set(0)
 
     # Added priority addons list
     del priority_addons_added[:]
-    
+
     # Checking for warmup in the priority addons list
     add_priority_addon('gg_warmup_round')
-    
+
     # Checking for warmup deathmatch
     if (int(gg_warmup_deathmatch) or int(gg_deathmatch)) and \
         not int(gg_warmup_elimination):
-        
+
         # Making sure elimination is off
         if int(gg_elimination):
             es.server.queuecmd('gg_elimination 0')            
-        
+
         # Enable gg_deathmatch
         if not int(gg_deathmatch):
             es.server.queuecmd('gg_deathmatch 1')
-        
+
         # Checking for deathmatch in the priority addons list
         add_priority_addon('gg_deathmatch')
-    
+
     # Checking for warmup elimination
     elif (int(gg_warmup_elimination) or int(gg_elimination)) and \
         not int(gg_warmup_deathmatch):
-        
+
         # Making sure deathmatch is off
         if int(gg_deathmatch):
             es.server.queuecmd('gg_deathmatch 0')   
-        
+
         # Enable gg_elimination
         if not int(gg_elimination):
             es.server.queuecmd('gg_elimination 1')
-    
+
         # Checking for elimination in the priority addons list
         add_priority_addon('gg_elimination')
 
@@ -273,46 +275,44 @@ def do_warmup(useBackupVars=True):
         warmupCountDown.stop()
         warmupCountDown.start(1, int(gg_warmup_timer) + GG_WARMUP_EXTRA_TIME)
         return
-            
+
     # Create a timer
     warmupCountDown = repeat.create('gungameWarmupTimer', count_down)
     warmupCountDown.start(1, int(gg_warmup_timer) + GG_WARMUP_EXTRA_TIME)
-    
+
 def count_down():
     warmupCountDown = repeat.find('gungameWarmupTimer')
-    
+
     # Making sure the count-down is going
     if not warmupCountDown:
         return
-        
+
     # If the remaining time is greater than 1
     if warmupCountDown['remaining'] >= 1:
-        
         # Send hint
         if warmupCountDown['remaining'] > 1:
             hudhint('#human', 'Timer_Plural', 
             {'time': warmupCountDown['remaining']})
         else:
             hudhint('#human', 'Timer_Singular')
-        
+
         # Countdown 5 or less?
         if warmupCountDown['remaining'] <= 5:
-            
             # Play beep
             play_beep()
-        
+
         # 1 second left in warmup
         if warmupCountDown['remaining'] == 1:
             # Stop the round from being able to end
             disable_round_end()
-            
+
             # Restart the round ourselves in 1 second
             es.server.queuecmd('mp_restartgame 1')
-            
+
             # Before the round ends up restarting, prepare gungame to be ready
             # for the first round of play
             gamethread.delayed(0.6, prepare_game)
-            
+
     # No time left
     elif warmupCountDown['remaining'] == 0:
         end_warmup('Timer_Ended')
@@ -335,7 +335,7 @@ def round_start(event_var):
     # If warmup is over
     if 'gg_warmup_round' in PriorityAddon():
         return
-    
+
     # Re-allow ending of rounds
     gpGameRules = None
 
@@ -348,24 +348,24 @@ def round_end(event_var):
 
 def disable_round_end():
     global gpGameRules
-    
+
     # Prepare gpGameRules to be able to properly prevent round_end in
     # round_end(event_var)
     if platform == 'nt':
         gpGameRules = spe.getPointer("\x8B\x0D\x2A\x2A\x2A\x2A\x85\xC9\x74\x2A\x8B\x01\x6A\x01\xFF\x50", 2)
     elif platform == 'posix':
         gpGameRules = spe.findSymbol("g_pGameRules")
-    
+
 def end_warmup(message):
     # Send hint
     hudhint('#human', message)
-    
+
     # Play beep
     play_beep()
-    
+
     # Delete the timer
     repeat.delete('gungameWarmupTimer')             
-    
+
     # Reset server vars back
     reset_server_vars(False)
 
@@ -374,8 +374,8 @@ def play_beep():
         Player(userid).playsound('countDownBeep')
 
 def reset_server_vars(unload):
-    # Warmup elimination ?
-    if int(gg_warmup_elimination):
+    # Warmup elimination or deathmatch previously loaded?
+    if int(gg_warmup_elimination) or int(gg_deathmatch_backup):
 
         # Back to DM ?
         if gg_deathmatch_backup:
@@ -390,9 +390,8 @@ def reset_server_vars(unload):
         elif not gg_elimination_backup:
             es.server.queuecmd('gg_elimination 0')
 
-    # Warmup DM ?
-    elif int(gg_warmup_deathmatch):
-
+    # Warmup DM or elimination previously loaded?
+    elif int(gg_warmup_deathmatch) or int(gg_elimination_backup):
         # Back to elimination ?
         if gg_elimination_backup:
             es.server.queuecmd('gg_deathmatch 0')
@@ -400,7 +399,7 @@ def reset_server_vars(unload):
             if unload:
                 gg_elimination.set(1)
             else:
-                es.server.queuecmd('gg_deathmatch 1')
+                es.server.queuecmd('gg_elimination 1')
 
         # Back to normal ?
         elif not gg_deathmatch_backup:
