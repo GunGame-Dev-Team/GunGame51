5b6a6b8e9639b2362d29c89edb72eb23c78d0cb4
WarrenAlpert <WarrenAlpert@efb7a570-fe77-11de-bcdd-59c7212cd493>
2010-01-24 03:29:48 +0000

    ../addons/eventscripts/gungame51/scripts/included/gg_knife_pro/gg_knife_pro.py: * Completely reworked and now uses gg_knife_pro_always_level.
    ../addons/eventscripts/gungame51/scripts/included/gg_knife_pro/gg_knife_pro_config.py:
    * Remove gg_knife_rookie.
    * Added gg_knife_pro_always_level and reworked explanations.

--- cstrike/addons/eventscripts/gungame51/scripts/included/gg_knife_pro/gg_knife_pro.py
+++ cstrike/addons/eventscripts/gungame51/scripts/included/gg_knife_pro/gg_knife_pro.py
@@ -16,7 +16,10 @@ import gamethread
 # GunGame Imports
 from gungame51.core.addons.shortcuts import AddonInfo
 from gungame51.core.players.shortcuts import Player
+from gungame51.core.players.shortcuts import add_attribute_callback
+from gungame51.core.players.shortcuts import remove_callbacks_for_addon
 from gungame51.core.weapons.shortcuts import get_total_levels
+from gungame51.core.weapons.shortcuts import get_level_weapon
 from gungame51.core.messaging.shortcuts import msg
 from gungame51.core.messaging.shortcuts import saytext2
 
@@ -27,8 +30,7 @@ info = AddonInfo()
 info.name = 'gg_knife_pro'
 info.title = 'GG Knife Pro' 
 info.author = 'GG Dev Team' 
-info.version = '0.1'
-info.conflicts = ['gg_knife_rookie']
+info.version = '0.2'
 info.translations = ['gg_knife_pro']
 
 # ============================================================================
@@ -36,143 +38,186 @@ info.translations = ['gg_knife_pro']
 # ============================================================================
 gg_knife_pro_limit = es.ServerVar('gg_knife_pro_limit')
 gg_allow_afk_levels = es.ServerVar('gg_allow_afk_levels')
-gg_knife_pro_rookie = es.ServerVar('gg_knife_pro_rookie')
+gg_allow_afk_levels_knife  = es.ServerVar('gg_allow_afk_levels_knife')
+gg_knife_pro_always_level = es.ServerVar('gg_knife_pro_always_level')
+
+# players level up internally before our player_death, so we added a callback
+# and store the userid who just got off of knife to check on in player_death
+recentlyOffKnife = []
 
 # ============================================================================
 # >> LOAD & UNLOAD
 # ============================================================================
 def load():
+    add_attribute_callback('level', level_call_back, info.name)
     es.dbgmsg(0, 'Loaded: %s' % info.name)
-    
+
 def unload():
     es.dbgmsg(0, 'Unloaded: %s' % info.name)
-    
+    remove_callbacks_for_addon(info.name)
+
 # ============================================================================
 # >> GAME EVENTS
 # ============================================================================
-def player_death(event_var):
-
-    # ===================
-    # Player Information
-    # ===================
-    attacker = int(event_var['attacker'])
-    userid   = int(event_var['userid'])
-    userteam = event_var['es_userteam']
-    attackerteam = event_var['es_attackerteam']
-
-    # ===================
-    # Check for suicide
-    # ===================
-    if (attackerteam == userteam) or (userid == attacker) or (attacker == 0):
+def level_call_back(name, value, ggPlayer):
+    # If the player is getting their level attribute set for the first time, we
+    # can't get it yet
+    if not hasattr(ggPlayer, "level"):
         return
-    
+
+    # If the player did not level up off of knife level, stop here
+    if get_level_weapon(ggPlayer.level) != "knife":
+        return
+
+    # Add the player to recentlyOffKnife for a short time so that we will know
+    # in player_death that they just leveled up off of knife level
+    recentlyOffKnife.append(ggPlayer.userid)
+    gamethread.delayed(0.2, recentlyOffKnife.remove, ggPlayer.userid)
+
+def player_death(event_var):
     # ===================
     # Was weapon a knife
     # ===================
     if event_var['weapon'] != 'knife':
         return
-   
+
     # ===================
-    # Attacker Info
+    # Player Information
     # ===================
+    attacker = int(event_var['attacker'])
+    victim   = int(event_var['userid'])
+    userteam = event_var['es_userteam']
+    attackerteam = event_var['es_attackerteam']
+    # ===================
+    # Check for suicide
+    # ===================
+    if (attackerteam == userteam) or (victim == attacker) or (attacker == 0):
+        return
+
     ggAttacker = Player(attacker)
-    
+    # gg_levelup fires before this because internal events fire first, so:
+    # If the player just got off of knife level, set their weapon to knife
+    attackerWeapon = "knife" if attacker in recentlyOffKnife else \
+                                                            ggAttacker.weapon
+
     # ===================
     # Attacker checks
     # ===================
-    # Fix duplicate winning
-    if ggAttacker.level >= get_total_levels():
-        return
-        
-    # Is the attacker on knife or grenade level?
-    if ggAttacker.weapon in ('knife', 'hegrenade'):
+    ggVictim = Player(victim)
+
+    # Is the victim AFK?
+    if ggVictim.afk():
+        # If we do not allow afk levelups through knife kills, stop here
+        if not (int(gg_allow_afk_levels) and int(gg_allow_afk_levels_knife)):
+            msg(attacker, 'VictimAFK', prefix=True)
+            return
+
+    # Never skip hegrenade level
+    if attackerWeapon == 'hegrenade':
+        # If gg_knife_pro_always_level is enabled, level down the victim
+        if int(gg_knife_pro_always_level):
+            level_down_victim(attacker, victim)
+
         msg(attacker, 'CannotSkipThisLevel', prefix=True)
         return
-    
-    # ===================
-    # Victim info
-    # ===================
-    ggVictim = Player(userid)
-    
+
+    # If the attacker is on knife level, stop here
+    if attackerWeapon == 'knife':
+        # If gg_knife_pro_always_level is enabled, level down the victim first
+        if int(gg_knife_pro_always_level):
+            level_down_victim(attacker, victim)
+
+        return
+
     # ===================
     # Victim checks
     # ===================
-        
     # Is victim on level 1?
     if ggVictim.level == 1:
-        
-        # Checking for knife rookie
-        if not int(gg_knife_pro_rookie):
+
+        # Checking for always level mode
+        if not int(gg_knife_pro_always_level):
             msg(attacker, 'VictimLevel1', prefix=True)
             return
-        
-    # Is the victim AFK?
-    if ggVictim.afk():
-        msg(attacker, 'VictimAFK', prefix=True)
-        return
-        
-    # Is the level difference higher than the limit?
-    if (ggAttacker.level - ggVictim.level) >= int(gg_knife_pro_limit) and \
-                                                int(gg_knife_pro_limit) != 0:
-        msg(attacker, 'LevelDifferenceLimit', 
-            {'limit': int(gg_knife_pro_limit)}, prefix=True)
-        return
-        
+
+    # If the level difference is higher than the limit
+    if is_out_of_limit(ggAttacker.level, ggVictim.level):
+        # If gg_knife_pro_always_level is off, stop here
+        if not int(gg_knife_pro_always_level):
+            msg(attacker, 'LevelDifferenceLimit', 
+                {'limit': int(gg_knife_pro_limit)}, prefix=True)
+            return
+
     # ===================
-    # Level changes
+    # Attacker Levelup
     # ===================
-    
-    # Can the victim level down ?
-    if ggVictim.level > 1:
-        
-        # Send message to attacker if victim cannot level down?
-        if ggVictim.preventlevel:
-            
-            # Knife rookie?
-            if not int(gg_knife_pro_rookie):
-                msg(attacker, 'VictimPreventLevel', prefix=True)
-        
-        # Level down the victim
-        else:   
-            
-            # Play sound & send message
-            ggVictim.playsound('leveldown')            
-            ggVictim.leveldown(1, attacker, 'steal')
-    
     # Can the attacker level up ?
     if not ggAttacker.preventlevel:
-        
+
+        # If the victim gets stopped by one of our checks before leveling down,
+        # still fire the steal event here because there was still a knife pro
+        # steal
+        if not level_down_victim(attacker, victim):
+            fire_gg_knife_steal(attacker, victim)
+
         # Play sound & levelup
         ggAttacker.playsound('levelsteal')
-        ggAttacker.levelup(1, userid, 'steal')
-    
-    # Prevent player from leveling twice from the same knife kill
-    ggAttacker.preventlevel.append('gg_knife_pro')
-    gamethread.delayed(0, ggAttacker.preventlevel.remove, ('gg_knife_pro'))
-    
-    
+        ggAttacker.levelup(1, victim, 'steal')
+
+def level_down_victim(attacker, victim):
+    ggAttacker = Player(attacker)
+    ggVictim = Player(victim)
+
+    # Can the victim level down ?
+    if ggVictim.level == 1:
+        return False
+
+    # Send message to attacker if victim cannot level down?
+    if ggVictim.preventlevel:
+
+        # Always level mode (do not bother the attacker)?
+        if not int(gg_knife_pro_always_level):
+            msg(attacker, 'VictimPreventLevel', prefix=True)
+
+            # The steal event didn't get fired
+            return False
+
+    # Level down the victim
+    # If the level difference is higher than the limit, stop here
+    if is_out_of_limit(ggAttacker.level, ggVictim.level):
+
+        # The steal event didn't get fired
+        return False
+
+    # Play sound & send message
+    ggVictim.playsound('leveldown')
+    ggVictim.leveldown(1, attacker, 'steal')
+
+    fire_gg_knife_steal(attacker, victim)
+    # The steal event got fired
+    return True
+
+def is_out_of_limit(attackerLevel, victimLevel):
+    # Is the level difference higher than the limit?
+    if (attackerLevel - victimLevel) > int(gg_knife_pro_limit) and \
+                                            int(gg_knife_pro_limit) != 0:
+        return True
+    return False
+
+def fire_gg_knife_steal(attacker, victim):
+    ggAttacker = Player(attacker)
+
     # ===================
     # Fire the event
     # ===================
     es.event('initialize', 'gg_knife_steal')
     es.event('setint', 'gg_knife_steal', 'attacker', attacker)
     es.event('setint', 'gg_knife_steal', 'attacker_level', ggAttacker.level)
-    es.event('setint', 'gg_knife_steal', 'userid_level', ggVictim.level)
-    es.event('setint', 'gg_knife_steal', 'userid', userid)
+    es.event('setint', 'gg_knife_steal', 'userid_level', Player(victim).level)
+    es.event('setint', 'gg_knife_steal', 'userid', victim)
     es.event('fire', 'gg_knife_steal')
-    
+
     # Announce the level steal
     saytext2('#human', ggAttacker.index, 'StoleLevel', 
-        {'attacker': event_var['es_attackername'], 
-        'victim': event_var['es_username']})
-
-
-
-
-
-
-
-
-
-
-
+        {'attacker': es.getplayername(attacker), 
+        'victim': es.getplayername(victim)})
--- cstrike/addons/eventscripts/gungame51/scripts/included/gg_knife_pro/gg_knife_pro_config.py
+++ cstrike/addons/eventscripts/gungame51/scripts/included/gg_knife_pro/gg_knife_pro_config.py
@@ -21,13 +21,13 @@ from gungame51.core.cfg import generate_header
 # ============================================================================
 config = cfglib.AddonCFG('%s/cfg/' %es.ServerVar('eventscripts_gamedir') +
     'gungame51/included_addon_configs/gg_knife_pro.cfg')
-        
+
 # ============================================================================
 # >> LOAD & UNLOAD
 # ============================================================================
 def load():
     generate_header(config)
-    
+
     # =========================================================================
     # GG_KNIFE_PRO CVARS
     # =========================================================================
@@ -36,66 +36,58 @@ def load():
     config.text('>> KNIFE PRO')
     config.text('='*76)
     config.text('Description:')
-    config.text('   When you kill a player with a knife, you will level up, ' +
+    config.text('    When you kill a player with a knife, you will level up, ' +
                 'and the victim')
     config.text('   will level down.')
-    config.text('Notes:')
-    config.text('   * Will not load with "gg_knife_rookie" enabled.')
-    config.text('   * See the variable "gg_knife_pro_limit" for further ' +
-                'enhancement of')
-    config.text('     gg_knife_pro.')
+    config.text('    The attacker will not steal a level if they are on ' +
+                'hegrenade or knife')
+    config.text('    level, or if the victim can\'t level down.')
     config.text('Options:')
-    config.text('   0 = (Disabled) Do not load gg_knife_pro.')
-    config.text('   1 = (Enabled) Load gg_knife_pro.')
+    config.text('    0 = (Disabled) Do not load gg_knife_pro.')
+    config.text('    1 = (Enabled) Load gg_knife_pro.')
     config.text('Default Value: 0')
     config.cvar('gg_knife_pro', 0, 'Enables/Disables ' + 
         'gg_knife_pro').addFlag('notify')
-    
+
     # Knife Pro Limit
     config.text('')
     config.text('='*76)
     config.text('>> KNIFE PRO LIMIT')
     config.text('='*76)
     config.text('Description:')
-    config.text('   Limits level stealing to players close to your own ' +
+    config.text('    Limits level stealing to players close to your own ' +
                 'level.')
     config.text('Example:')
-    config.text('   * If this is set to 3, you will not gain a level if you ' +
+    config.text('    If this is set to 3, you will not gain a level if you ' +
                 'knife someone')
-    config.text('     more than 3 levels below you.')
+    config.text('    more than 3 levels below you.')
     config.text('Options:')
-    config.text('   0 = (Disabled) Do not enable the knife pro limit.')
-    config.text('   # = (Enabled) Limit level stealing to this # of levels ' +
+    config.text('    0 = (Disabled) Do not enable the knife pro limit.')
+    config.text('    # = (Enabled) Limit level stealing to this # of levels ' +
                 'below the')
     config.text('                 attacker.')
     config.text('Default Value: 0')
     config.cvar('gg_knife_pro_limit', 0, 'Limit level stealing to this # of ' +
                 'levels below the attacker.')
-    
-    # Knife Rookie
+
+    # Knife Pro Always Level
     config.text('')
     config.text('='*76)
-    config.text('>> KNIFE PRO ROOKIE')
+    config.text('>> KNIFE PRO ALWAYS LEVEL')
     config.text('='*76)
     config.text('Description:')
-    config.text('      * The attacker will level up even when the victim is ' +
-                'on level 1.')
-    config.text('      * The attacker will level up even though the victim ' +
-                'cannot leveldown.')
-    config.text('      * The victim will level down even though the attacker' +
-                ' cannot levelup.')
-    config.text('Notes:')
-    config.text('   * Allows attackers to level up even if the victim is on' +
-                ' level 1.')
+    config.text('    The attacker will always level up unless they are on ' +
+                'hegrenade level.')
+    config.text('    The vitim will always level down unless ' +
+                'gg_knife_pro_limit takes effect.')
     config.text('Options:')
-    config.text('   0 = (Disabled) Do not allow attackers to level up if the' +
-                ' victim is on')
-    config.text('                  level 1.')
-    config.text('   1 = (Enabled) Allow attackers to level up if the victim ' +
-                'is on level 1.')
+    config.text('    0 = (Disabled) Conform to logical gg_knife_pro ruling')
+    config.text('    1 = (Enabled) Always affect levels with exception to the' +
+                ' situations in')
+    config.text('                   the description above.')
     config.text('Default Value: 0')
-    config.cvar('gg_knife_pro_rookie', 0, 'Enables/Disables stealing levels ' +
-                'from victims on level 1.')
+    config.cvar('gg_knife_pro_always_level', 0, 'Enables/Disables always ' +
+                'stealing levels.')
 
     config.write()
     es.dbgmsg(0, '\tgg_knife_pro.cfg')
@@ -106,6 +98,6 @@ def unload():
     # Remove the "notify" flags as set by addFlag('notify')
     for cvar in config.getCvars().keys():
         es.flags('remove', 'notify', cvar)
-    
+
     # Delete the cfglib.AddonCFG instance
     del config
