ee270c78bb99017814e5ca6de2563a3bf631b210
jlukerobi <jlukerobi@efb7a570-fe77-11de-bcdd-59c7212cd493>
2010-09-09 17:17:37 +0000

    ./addons/eventscripts/gungame51/core/events/__init__.py     * Properly identified recentWinner as a global to stop the startup warning

--- cstrike/addons/eventscripts/gungame51/core/events/__init__.py
+++ cstrike/addons/eventscripts/gungame51/core/events/__init__.py
@@ -60,11 +60,12 @@ class EventManager(object):
         
         # Check to see if the player just won
         if newLevel > get_total_levels():
+            global recentWinner
+            
             # If there was a recentWinner, stop here
             if recentWinner:
                 return False
-            
-            global recentWinner
+        
             # Set recentWinner to True
             recentWinner = True
             # In 1 second, remove the recentWinner
